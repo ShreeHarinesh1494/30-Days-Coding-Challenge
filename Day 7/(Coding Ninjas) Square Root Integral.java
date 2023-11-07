7d4ace6ae109504bd968aec0c5f11f6f7af0ce62
Shree Harinesh S <117459133+ShreeHarinesh1494@users.noreply.github.com>
2023-11-07 20:30:23 +0530

    Create (Coding Ninjas) Square Root Integral.java

---
+++ Day 7/(Coding Ninjas) Square Root Integral.java
@@ -0,0 +1,16 @@
+Problem : Find Square Root of No N in integral
+
+Solution :
+import java.util.*;
+import java.lang.Math;
+public class Main {
+	
+	public static void main(String[] args)
+	 {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		double d=Math.sqrt(n);
+		System.out.println((int)d); 
+
+	}
+}
