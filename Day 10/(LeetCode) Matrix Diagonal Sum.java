d73e3ed8a592f9abfc4b1b5cf6a2045ec0689a65
Shree Harinesh S <117459133+ShreeHarinesh1494@users.noreply.github.com>
2023-11-10 20:22:42 +0530

    Create (LeetCode) Matrix Diagonal Sum.java

---
+++ Day 10/(LeetCode) Matrix Diagonal Sum.java
@@ -0,0 +1,21 @@
+Problem :
+Given a square matrix mat, return the sum of the matrix diagonals.
+Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+
+Solution :
+  class Solution {
+    public int diagonalSum(int[][] mat) 
+    {
+        int sum=0;
+        int n=mat.length;
+        for(int i=0;i<n;i++)
+        {
+            sum+=mat[i][i]+mat[i][n-i-1];
+        }
+        if(n%2==1)
+        {
+            sum-=mat[n/2][n/2];
+        }
+        return sum;
+    }
+}
