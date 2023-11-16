f91f68981d393bfd56d083824b9386c8ae79a3d0
Shree Harinesh S <117459133+ShreeHarinesh1494@users.noreply.github.com>
2023-11-16 14:29:52 +0530

    Create (Coding Ninjas) Search In Rotated Sorted Array.java

---
+++ Day 16/(Coding Ninjas) Search In Rotated Sorted Array.java
@@ -0,0 +1,44 @@
+Problem :
+Search In Rotated Sorted Array
+
+Solution :
+
+  public class Solution {
+    public static int search(int arr[], int key) 
+    {
+        int n=arr.length;
+        int low=0;
+        int high=n-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(arr[mid]==key)
+            {
+                return mid;
+            }
+            if(arr[low]<=arr[mid])
+            {
+                if(arr[low]<= key && key <=arr[mid])
+                {
+                    high=mid-1;
+                }
+                else
+                {
+                    low=mid+1;
+                }
+            }
+            else
+            {
+                if(arr[mid] <= key && key<=arr[high])
+                {
+                    low=mid+1;
+                }
+                else
+                {
+                    high=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
