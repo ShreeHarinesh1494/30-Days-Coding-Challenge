4e173caaddee9dbd36539e69d8b9ab0de2136920
Shree Harinesh S <117459133+ShreeHarinesh1494@users.noreply.github.com>
2023-11-21 19:08:14 +0530

    Create (Coding Ninjas) Kth Smallest and Largest Element of Array.java

---
+++ Day 21/(Coding Ninjas) Kth Smallest and Largest Element of Array.java
@@ -0,0 +1,20 @@
+Problem:
+Kth Smallest and Largest Element of ArrayList
+
+Solution :
+
+  import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class Solution {
+	public static ArrayList<Integer> kthSmallLarge(ArrayList<Integer> arr, int n, int k) 
+	{
+		ArrayList<Integer> ans = new ArrayList<>();
+
+		Collections.sort(arr);
+		ans.add(arr.get(k-1));
+		ans.add(arr.get(n-k));
+		return ans;
+	}
+}
