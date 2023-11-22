2ef7cda772a8845f3a0d0042b686295c59fd46f4
Shree Harinesh S <117459133+ShreeHarinesh1494@users.noreply.github.com>
2023-11-22 19:03:26 +0530

    Create (LeetCode) Valid Palindrome.java

---
+++ Day 22/(LeetCode) Valid Palindrome.java
@@ -0,0 +1,27 @@
+Problem:
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. 
+Alphanumeric characters include letters and numbers.
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Solution :
+
+  class Solution {
+    public boolean isPalindrome(String s) 
+    {
+       s=s.toLowerCase().replaceAll("[^a-zA-Z0-9]","");
+       int i=0;
+       int j=s.length()-1;
+
+       while(i<j)
+       {
+           if(s.charAt(i)!=s.charAt(j))
+           {
+               return false;
+           }
+           i++;
+           j--;
+       } 
+       return true;
+    }
+}
