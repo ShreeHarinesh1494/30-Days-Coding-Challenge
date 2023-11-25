2c8cbfe848c32d61a091168df5b4db860b5f0630
Shree Harinesh S <117459133+ShreeHarinesh1494@users.noreply.github.com>
2023-11-25 19:26:25 +0530

    Create (Geeks For Geeks) Binary Array Sorting.java

---
+++ Day 25/(Geeks For Geeks) Binary Array Sorting.java
@@ -0,0 +1,16 @@
+Problem :
+
+Given a binary array A[] of size N. The task is to arrange the array in increasing order.
+Note: The binary array contains only 0  and 1.
+
+Solution :
+
+class Solution
+{
+    //Function to sort the binary array.
+    static void binSort(int A[], int N)
+    {
+        Arrays.sort(A);
+    }
+}
+  
